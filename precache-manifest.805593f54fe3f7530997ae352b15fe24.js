1b8072985abf1eaeecf82b185ac627f78cb47c77
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-13 15:39:13 +0000

    Deploy mk-hash/nxt2learn-react-app to github.com/mk-hash/nxt2learn-react-app.git:gh-pages

---
+++ precache-manifest.805593f54fe3f7530997ae352b15fe24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b261a2e150d9157c4b10903669a95a",
+    "url": "/nxt2learn-react-app/index.html"
+  },
+  {
+    "revision": "185ade91d5992ac8b5e3",
+    "url": "/nxt2learn-react-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d7f32d25b2f749dfe329",
+    "url": "/nxt2learn-react-app/static/js/2.88466500.chunk.js"
+  },
+  {
+    "revision": "185ade91d5992ac8b5e3",
+    "url": "/nxt2learn-react-app/static/js/main.8f9ab579.chunk.js"
+  },
+  {
+    "revision": "f4bc3cbf9ceabb2f78d4",
+    "url": "/nxt2learn-react-app/static/js/runtime-main.5e126264.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/nxt2learn-react-app/static/media/logo.25bf045c.svg"
+  }
+]);
